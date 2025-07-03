b6b4824a0573e7769e363fe5c62e343e8a5dcf0a
HOME-PC\requiem <farrvet1@mail.ru>
2025-07-03 12:54:04 +0300

    Task 2: completed

--- CyberSportsPortal.Core/OlympiadServices/TeamTasksService.cs
+++ CyberSportsPortal.Core/OlympiadServices/TeamTasksService.cs
@@ -1,5 +1,6 @@
 ﻿using CyberSportsPortal.Data.Entities;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CyberSportsPortal.Core.OlympiadServices;
 
@@ -7,8 +8,30 @@ public class TeamTasksService
 {
     public int GetTeamIncomeForYear(Team team, int year)
     {
-        return 100;
+        int totalIncome = 0;
+
+        foreach (TournamentParticipantInfo result in team.TeamTournamentResults)
+        {
+            Tournament tournament = result.Tournament;
+            if (tournament == null || result.Place == null)
+                continue;
+
+            if (tournament.EndDate.Year != year)
+                continue;
+
+            TournamentPrize prize = tournament.TournamentPrizes
+                ?.FirstOrDefault(p => p.Place == result.Place.Value);
+
+            if (prize != null)
+            {
+                totalIncome += prize.Prize;
+            }
+        }
+
+        return totalIncome;
+
     }
+
 
     public List<Rating> GetNewRatings(List<MatchHistory> matches, List<Rating> oldRatings)
     {
